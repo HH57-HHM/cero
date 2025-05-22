2778186de34e97e257ee22832ac78adfa5f782b7
hugo <hugohm070105hp@gmail.com>
2025-05-22 13:25:53 -0600

    primer cambio en index.js

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use(express.json());
 app.get("/temperatura", (req, res) => {
   res.json({ valor: "10 °C", timestamp: new Date().toISOString() });
 });
+
+app.get("/hh57", (req, res) => {
+  res.json({ nombre: "hugo", apellido: "heredia" });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
